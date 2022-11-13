21dae58d101493ca756eec64cc82cde5a794ce87
hussaindiwan52 <hussaindiwan52@gmail.com>
2022-11-14 03:58:10 +0530

    ...

--- track_my_cash/client/src/components/Pie_individual.js
+++ track_my_cash/client/src/components/Pie_individual.js
@@ -1,36 +1,39 @@
 import React from "react";
 import axios from "axios";
-import { Doughnut } from 'react-chartjs-2';
+import { Doughnut } from "react-chartjs-2";
 import { useState } from "react";
 
 export default function Pie_individual(props) {
-	
 	const data = {
 		labels: props.labels,
 		datasets: [
-		  {
-			label: props.heading,
-			data: props.values,
-			backgroundColor: [
-			  'rgba(255, 99, 132, 0.2)',
-			  'rgba(54, 162, 235, 0.2)',
-			  'rgba(255, 206, 86, 0.2)',
-			  'rgba(75, 192, 192, 0.2)',
-			  'rgba(153, 102, 255, 0.2)',
-			  'rgba(255, 159, 64, 0.2)',
-			],
-			borderColor: [
-			  'rgba(255, 99, 132, 1)',
-			  'rgba(54, 162, 235, 1)',
-			  'rgba(255, 206, 86, 1)',
-			  'rgba(75, 192, 192, 1)',
-			  'rgba(153, 102, 255, 1)',
-			  'rgba(255, 159, 64, 1)',
-			],
-			borderWidth: 3,
-		  },
+			{
+				label: props.heading,
+				data: props.values,
+				backgroundColor: [
+					"rgba(255, 99, 132, 0.2)",
+					"rgba(54, 162, 255, 0.2)",
+					"rgba(255, 200, 86, 0.2)",
+					"rgba(175, 152, 102, 0.2)",
+					"rgba(153, 102, 255, 0.2)",
+					"rgba(255, 159, 64, 0.2)",
+					"rgba(120, 69, 150, 0.2)",
+					"rgba(10, 40, 120, 0.2)",
+				],
+				borderColor: [
+					"rgba(255, 99, 132, 1)",
+					"rgba(54, 162, 255, 1)",
+					"rgba(255, 200, 86, 1)",
+					"rgba(175, 152, 102, 1)",
+					"rgba(153, 102, 255, 1)",
+					"rgba(255, 159, 64, 1)",
+					"rgba(120, 69, 150, 1)",
+					"rgba(10, 40, 120, 1)",
+				],
+				borderWidth: 3,
+			},
 		],
-	  };
+	};
 	const iid = props.iid;
 
 	return (
@@ -59,10 +62,8 @@ export default function Pie_individual(props) {
 						</div>
 
 						<div class="modal-body ">
-                        <Doughnut data={data} />
+							<Doughnut data={data} />
 						</div>
-
-						
 					</div>
 				</div>
 			</div>
