ca478a0eea2f6803dcab0c78193921d5aa2c7a75
Aniket979Kanodia <kanodiaaniket979@gmail.com>
2022-11-14 14:37:40 +0530

    Report Completed

--- track_my_cash/client/src/components/Group_Report.js
+++ track_my_cash/client/src/components/Group_Report.js
@@ -3,26 +3,44 @@ import Sidebar from "./Sidebar";
 import axios from "axios";
 import Cookies from "universal-cookie";
 import { Navigate, useNavigate } from "react-router-dom";
-
+import Report_Card from "./report_card";
 
 const Group_Report = ()=>{
 
     const cookies = new Cookies();
+    const [membersExpenses, setExpenses] = useState([]);
+
 	const Mem_Id = cookies.get("Member").mem_id;
 	const fetchgroupDues= async (e) => {
         await axios
 			.get("http://localhost:5000/member/dues/" + Mem_Id)
 			.then((res) => {
-				console.log(res.data);
+                console.log(res.data);
+				setExpenses(res.data);
 			});
     };
     useEffect(() => {
     fetchgroupDues();}, []);
         return (
     
-    <React.Fragment>
-report
-    </React.Fragment>
+            <React.Fragment>
+			<div>
+				<Sidebar />
+
+				
+
+				<div className="container mt-4">
+					
+					<div className="card mt-4">
+						<div className="card-header">Expenses</div>
+						{membersExpenses.map((ele) => (
+							<Report_Card content={ele}></Report_Card>
+						))}
+					</div>
+					<button>Log Out</button>
+				</div>
+			</div>
+		</React.Fragment>
 
 )
 };
