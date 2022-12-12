8cad3f4f839e591e6e6d314ffc4d7ada9bed036c
Aniket979Kanodia <kanodiaaniket979@gmail.com>
2022-12-13 02:43:40 +0530

    Temp2

--- track_my_cash/server/index.js
+++ track_my_cash/server/index.js
@@ -10,6 +10,7 @@ import individualRoutes from "./routes/individual.js";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // app.use(
 // 	cors({
